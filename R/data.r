6163863b4a2762a981adfcaa9069e26a4555bb83
CharlieOuthwaite <charloteouthwaite@gmail.com>
2019-06-17 17:06:25 +0100

    Created data r script for the major_groups table.  Followed instructions in Hadley Wickham's book.

---
+++ R/data.r
@@ -0,0 +1,13 @@
+#' Details of major group of each species.
+#'
+#' A dataset containing each species name and the associated taxa and major_group.
+#' This dataset is necessary for generating the group level analyses.
+#'
+#' @format A data frame with 5293 rows and 3 variables:
+#' \describe{
+#'   \item{Group}{Taxa name, 32 levels}
+#'   \item{Species}{Species name}
+#'   \item{Major_group}{Major group, e.g. FREHWATER_SPECIES, 4 levels}
+#'   ...
+#' }
+"Major_groups"
