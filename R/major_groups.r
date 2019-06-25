6af05c21aef95afbe7ee39c85e298266f26f2ece
CharlieOuthwaite <charloteouthwaite@gmail.com>
2019-06-25 12:28:48 +0100

    Adding required dataset information and including as internal data as well.

---
+++ R/major_groups.r
@@ -0,0 +1,14 @@
+#' Major group assignment for each species
+#'
+#' A dataset containing information on which major group (e.g. FRESHWATER_SPECIES
+#'  or "LOWER_PLANTS") each species has been assigned to for the analysis carried
+#'  out in the paper by Outhwaite et al.
+#'
+#' @format A data frame with 5293 rows and 3 variables:
+#' \describe{
+#'   \item{Group}{Taxonomic group}
+#'   \item{Species}{Species name}
+#'   \item{Major_group}{Major group}
+#'   ...
+#' }
+"major_groups"
