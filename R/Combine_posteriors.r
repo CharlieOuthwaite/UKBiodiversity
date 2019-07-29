32f17d881aa15cfad19908a18857c894d0b2a135
CharlieOuthwaite <charlotteouthwaite@gmail.com>
2019-07-29 14:14:34 +0100

    Updated to remove the Rove beetles

--- R/Combine_posteriors.r
+++ R/Combine_posteriors.r
@@ -40,8 +40,13 @@ combine_posteriors <- function(group_level, datadir, outdir, status = TRUE){
   # check for correct group level specification
   if(!group_level == "taxa" & !group_level == "major_group") stop("group_level must be taxa or major_group")
 
+
 # load the major groups list
 major_groups <- as.data.frame(major_groups)
+
+# Remove Rove beetles from the major_groups table.
+# These are not analyses here since data starts in 1980 rather than 1970.
+major_groups <- major_groups[!major_groups$Group == "RoveBeetles", ]
 
 # remove brackets from list and outputs
 major_groups$Species <- sub("\\(", "", major_groups$Species)
