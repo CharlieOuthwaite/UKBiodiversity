2ca794f48644e40bc37bbf3d1fab492b4cf21e2e
CharlieOuthwaite <charlotteouthwaite@gmail.com>
2019-07-30 14:02:34 +0100

    alternative plot format for fig 4

--- R/generate_fig4.r
+++ R/generate_fig4.r
@@ -206,11 +206,11 @@ ggsave(filename = paste0(outdir, "/Figure_4.pdf"), height = 10, width = 16)
 }
 
 
-return(plot_grid(p[[2]], p[[1]], p[[4]], p[[3]], align = "hv", ncol = 1,
+return(plot_grid(p[[2]], p[[4]], p[[3]], p[[1]], align = "hv", ncol = 1,
                  labels = c("Freshwater Species",
                             "Insects",
-                            "Inverts",
-                            "Bryophytes & Lichens"),
+                            "Bryophytes & Lichens",
+                            "Inverts"),
                  hjust = 0, label_size = 12, label_x = 0.1))
 
 }
